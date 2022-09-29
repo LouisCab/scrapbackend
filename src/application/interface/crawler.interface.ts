9c522ca9032a8de76b8183e25c9aa2cd4434ec87
LouisCab <lo.cabirol@gmail.com>
2022-09-29 11:06:15 +0200

    wip

--- src/application/interface/crawler.interface.ts
+++ src/application/interface/crawler.interface.ts
@@ -1,7 +1,47 @@
+import * as puppeteer from 'puppeteer';
 export type RawElement = string;
+
+export class PageRedirectionFailed extends Error {
+  constructor(url: string) {
+    super(`Failed to redirect to ${url}`);
+  }
+}
+
+export class CookiesAcceptanceFailed extends Error {
+  constructor(selector: string) {
+    super(`Failed to accept cookies for ${selector}, hint : maybe add a delay`);
+  }
+}
 export abstract class Crawler {
-  abstract goto(url: string): Promise<void>;
-  abstract consentCookies(selector: string): void;
-  abstract gotoFirstResult(selector: string): void;
+  protected page: puppeteer.Page;
+  private elements: Promise<string>;
+
+  get puppeteerPage() {
+    return this.page;
+  }
+
+  get puppeteerElement() {
+    return this.elements;
+  }
+
+  set setPuppeteerElement(elements: Promise<string>) {
+    this.elements = elements;
+  }
+  constructor(page: puppeteer.Page) {
+    this.page = page;
+  }
+  async goto(url: string) {
+    await this.page.goto(url);
+    if (this.page.url() != url) {
+      throw new PageRedirectionFailed(url);
+    }
+  }
+  async consentCookies(selector: string) {
+    await this.page.waitForSelector(selector);
+    await this.page.click(selector);
+  }
+  async gotoFirstResult(selector: string) {
+    await this.page.click(selector);
+  }
   abstract getRawElement(identifier: string): Promise<RawElement[]>;
 }
--- src/infrastructure/crawler/societe-com/societe-com.crawler.ts
+++ src/infrastructure/crawler/societe-com/societe-com.crawler.ts
@@ -4,52 +4,10 @@ import {
   RawElement,
 } from '../../../application/interface/crawler.interface';
 
-export class PageRedirectionFailed extends Error {
-  constructor(url: string) {
-    super(`Failed to redirect to ${url}`);
-  }
-}
-
-export class CookiesAcceptanceFailed extends Error {
-  constructor(selector: string) {
-    super(`Failed to accept cookies for ${selector}, hint : maybe add a delay`);
-  }
-}
 export class SocieteComCrawler extends Crawler {
-  private page: puppeteer.Page;
-  private elements: Promise<string>;
-
-  get puppeteerPage() {
-    return this.page;
-  }
-
-  get puppeteerElement() {
-    return this.elements;
-  }
-
-  set setPuppeteerElement(elements: Promise<string>) {
-    this.elements = elements;
-  }
-
   constructor(page: puppeteer.Page) {
-    super();
+    super(page);
     this.page = page;
-  }
-
-  async goto(url: string) {
-    await this.page.goto(url);
-    if (this.page.url() != url) {
-      throw new PageRedirectionFailed(url);
-    }
-  }
-
-  async consentCookies(selector: string) {
-    await this.page.waitForSelector(selector);
-    await this.page.click(selector);
-  }
-
-  async gotoFirstResult(selector: string) {
-    await this.page.click(selector);
   }
 
   async getRawElement(selector: string): Promise<RawElement[]> {
