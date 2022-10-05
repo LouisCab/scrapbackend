7c3fd99613f4e4a508166b704df034c0f57674e5
LouisCab <lo.cabirol@gmail.com>
2022-10-05 15:20:22 +0200

    wip

--- src/infrastructure/information-provider/linkedin.information-provider.spec.ts
+++ src/infrastructure/information-provider/linkedin.information-provider.spec.ts
@@ -1,12 +1,33 @@
+import { NoResultForInput } from '../information-crawler/information-crawler';
+import { LinkedinInformationCrawler } from '../information-crawler/linkedin.information-crawler';
+import { SocieteComInformationCrawler } from '../information-crawler/societe-com.information-crawler';
+import { PuppeteerInformationExtractor } from './information-extractor/puppeter.information-extractor';
+import { linkedinInformationLocator } from './information-referential/linkedin.information-referential';
+import { PuppeteerInformationRefinery } from './information-refinery/puppeteer-information-refinery';
 import { LinkedinProvider } from './linkedin.information-provider';
 
-describe('Linkedin crawler', () => {
-  it('should retrieve raw page of specified company', async () => {
-    const provider = new LinkedinProvider();
-    const elements = await provider.getElementsCompanyInfomations('gojob');
+let provider: LinkedinProvider;
+let extractor: PuppeteerInformationExtractor;
+let transformer: PuppeteerInformationRefinery;
+let crawler: SocieteComInformationCrawler;
+const referential = linkedinInformationLocator;
 
+beforeEach(() => {
+  transformer = new PuppeteerInformationRefinery();
+  crawler = new LinkedinInformationCrawler();
+  extractor = new PuppeteerInformationExtractor(crawler);
+  provider = new LinkedinProvider(extractor, transformer, crawler, referential);
+});
+describe('Linkedin provider', () => {
+  it('should retrieve all aimed information on page for specified company', async () => {
+    const elements = await provider.getElementsCompanyInfomations('gojob');
     expect(elements).toBeDefined();
-    console.log(elements);
-    expect(elements.length).toBe(6);
+    expect(Object.entries(elements).length).toBe(6);
+  });
+
+  it('should throw an error when no result is given on google', async () => {
+    await expect(async () =>
+      provider.getElementsCompanyInfomations('ozienhoznhbeozebnoeibnoze'),
+    ).rejects.toThrow(NoResultForInput);
   });
 });
--- src/infrastructure/information-provider/linkedin.information-provider.ts
+++ src/infrastructure/information-provider/linkedin.information-provider.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@nestjs/common';
-
 import { InformationReferential } from '../../application/information-referential';
 import { InformationProvider } from '../../application/interface/information-provider/information-provider.interface';
 import { constants } from '../../constants';
 import { CompanyInformation } from '../../domain/company';
 import { LinkedinInformationCrawler } from '../information-crawler/linkedin.information-crawler';
-import { PuppeteerInformationExtractor } from './_information-extractor/_puppeteer.information-extractor';
-import { PuppeteerInformationLocator } from './information-extractor/puppeter.information-extractor';
+import { PuppeteerInformationExtractor } from './information-extractor/puppeter.information-extractor';
+import { PuppeteerInformationRefinery } from './information-refinery/puppeteer-information-refinery';
 
 @Injectable()
 export class LinkedinProvider extends InformationProvider<CompanyInformation> {
   constructor(
-    // private readonly locator: PuppeteerInformationLocator,
     private readonly extractor: PuppeteerInformationExtractor,
+    private readonly transformer: PuppeteerInformationRefinery,
     private readonly crawler: LinkedinInformationCrawler,
     private readonly referential: InformationReferential,
   ) {
@@ -21,18 +20,22 @@ export class LinkedinProvider extends InformationProvider<CompanyInformation> {
 
   async getElementsCompanyInfomations(
     companyName: string,
-  ): Promise<CompanyInformation[]> {
+  ): Promise<CompanyInformation> {
     await this.crawler.initBrowser();
     await this.crawler.goto(constants.GOOGLE_SEARCH_LINKEDIN + companyName);
     await this.crawler.consentCookies(constants.GOOGLE_CONSENT);
     await this.crawler.gotoFirstResult(constants.GOOGLE_FIRST_RESULT);
 
-    const elements = await this.extractor.extractRawData();
+    const rawElements = await this.extractor.extractRawData(
+      this.referential.informationRubrics,
+    );
 
-    const companyInformations = await this.extractor.extractRawData(elements);
+    const companyInformations = await this.transformer.transformRawData(
+      rawElements,
+    );
 
     await this.crawler.closeBrowser();
 
-    return informations;
+    return companyInformations;
   }
 }
