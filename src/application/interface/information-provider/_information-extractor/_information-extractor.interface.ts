71b57a52f0f2beabc28df137acfeabc298cce2cc
LouisCab <lo.cabirol@gmail.com>
2022-10-05 15:23:12 +0200

    deletion

--- src/application/interface/information-provider/_information-extractor/_information-extractor.interface.ts
+++
@@ -1,9 +0,0 @@
-import { CompanyInformation } from '../../../../domain/company';
-import { InformationRubricDefinition } from '../../../information-referential';
-
-interface InformationExtractor2 {
-  extractRawData(
-    locator: InformationRubricDefinition,
-    value: string,
-  ): CompanyInformation;
-}
